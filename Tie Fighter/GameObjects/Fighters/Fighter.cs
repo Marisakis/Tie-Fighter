f07ad510c9dfac88e71943b87cedb3fa5e3fa0fa
Dustin <dhendrik6@avans.nl>
2019-10-06 14:42:56 +0200

    GameObject class added following the design.
    Added GameObject class containing the most basic and generic implementation as described in our Lucidchart document.

---
+++ Tie Fighter/GameObjects/Fighters/Fighter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tie_Fighter.GameObjects
+{
+    public class Fighter<T> : GameObject<T>
+    {
+        public T ttp { get; set; } // Time to pass, in milliseconds.
+    }
+}
---
+++ Tie Fighter/GameObjects/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tie_Fighter.GameObjects
+{
+    public abstract class GameObject<T>
+    {
+        public virtual T x { get; set; } // X-position.
+        public virtual T y { get; set; } // Y-position.
+        public virtual Tuple<T, T> GetPosition()
+        {
+            return new Tuple<T, T>(x, y);
+        }
+
+    }
+}
