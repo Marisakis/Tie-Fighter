571743d3f158c2f1348b89bd54a3a942156d121f
Marisakis <sebcolijn@live.nl>
2019-10-15 10:46:32 +0200

    Reset

--- Tie Fighter/Controllers/IActionInput.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Tie_Fighter.Controllers
-{
-    public interface IActionInput<T>
-    {
-        void MoveTo(T x, T y);
-        void UpdatePosition(T x, T y);
-        void Fire();
-    }
-}
