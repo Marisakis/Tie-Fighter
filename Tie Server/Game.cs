d9691a57bdff9c4b8bab3551222417b8f4a1d504
Marisakis <sebcolijn@live.nl>
2019-10-18 14:24:31 +0200

    Highscore struct and write/read operations
    untested

--- Tie Server/Game.cs
+++ Tie Server/Game.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tie_Server
 {
+    /// <summary>
+    /// Class that contains a game and 
+    /// </summary>
     class Game
     {
         private GameManager gameManager;
@@ -16,14 +20,45 @@ namespace Tie_Server
         }
 
 
-        private void Stop()
+        private void Finish()
         {
-
         }
 
-        public Player GetHighestScore()
+
+        public HighScore GetHighestScore(List<Player> players)
         {
-            throw new NotImplementedException();
+            //PLACEHOLDER CODE, REPLACE LATER
+            Player highestScore = null;
+            return new HighScore();
+        }
+
+        public static List<HighScore> getHighScoresFromFile()
+        {
+            string path = Directory.GetCurrentDirectory();
+            path += "highscores.txt";
+            List<HighScore> highscores = new List<HighScore>();
+            if (!File.Exists(path))
+                return highscores;
+            else
+            {
+                using (FileStream fileStream = new FileStream(path, FileMode.Open))
+                {
+                    var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                    highscores = (List<HighScore>)binaryFormatter.Deserialize(fileStream);
+                }
+                return highscores;
+            }
+        }
+
+        public static void writeHighscoresToFile(List<HighScore> highscores)
+        {
+            string path = Directory.GetCurrentDirectory();
+            path += "highscores.txt";
+            using (Stream stream = File.Open(path, FileMode.Create))
+            {
+                var binaryFormatter = new System.Runtime.Serialization.Formatters.Binary.BinaryFormatter();
+                binaryFormatter.Serialize(stream, highscores);
+            }
         }
     }
 }
